d39baac9acb2ff11eceaebbaf664ff042eb5637f
Rodrigo Paiva <rodrigonpaiva@hotmail.com>
2023-05-17 11:07:11 +0200

    Slideshow - Create and editing

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,11 @@ module.exports = {
       lg: '976px',
       xl: '1440px',
     },
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'car-slide-show': "url('/src/images/car-slideshow.png')",
+      }
+    },
   },
   variantes:{
     extends:{
